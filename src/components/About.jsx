172647a1628ad3210243c6dad735bfca6dc70755
thristan gabriel pereira deretti <thristanxyz@gmail.com>
2022-08-02 17:40:52 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
 
                 <p className='text-xl'>
                     Hello! my name is Thristan Gabriel, I’m  19 years old and i was born in Brasil.
-                    My first contact with programming was when I was 15 years old as a intern in a startup called Clinicorp, in those years I was responsible for projects such as recurring charge, financial data import, redesign of screens and others. Nowadays I’m in a small team called Fintech at Clinicorp, using a Baas(Banking as a service) in the background of the project to deliver a complete financial solution to increase the revenue of our customers.
+                    My first contact with programming was when I was 15 years old as a intern in a startup called Clinicorp, in those years I was responsible for projects such as recurring billing, financial data import, redesign of screens and others. Nowadays I’m in a small team called Fintech at Clinicorp, using a Baas(Banking as a service) in the background of the project to deliver a complete financial solution to increase the revenue of our customers.
                 </p>
             </div>
             
