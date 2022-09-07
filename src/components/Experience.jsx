135569657f72a72fbbd5a37d88572bd31f10372b
thristan <thristan.gabriel@clinicorp.com>
2022-09-07 15:49:29 -0300

    save

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -21,16 +21,18 @@ function Experience() {
     ];
 
     return (
-        <div name='experience' className=" w-full h-screen bg-gradient-to-b from-gray-800 to-black">
+        <div name='experience' className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
 
             <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
                 
                 <div>
-                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Experience</p>
-                    <p className='py-6'>these are the technologies I've worked with</p>
+                    <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+                        Experience
+                    </p>
+                    <p className="py-6">These are the technologies I've worked with</p>
                 </div>
 
-                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
                     {experiences.map(({id, src, title, style}) => (
                         <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
                             <img src={src} alt="html" className='w-20 mx-auto' />
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import thristan_img from '../assets/thristan_img.jpeg';
+import thristan_img from '../assets/tailwind.png';
 
 function Portfolio() {
 
     const portfolio = [
         {
-            id:1,
+            id: 1,
             src: thristan_img
         },
         {
@@ -23,16 +23,17 @@ function Portfolio() {
     ];
 
     return (
-        <div name='portfolio' className='bg-gradient-to-b from-black via-black to-gray-800 w-full text-white md:h-screen'>
-
-            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-                    <p className='py-6'>Check out some of my work right here</p>
+        <div name='portfolio' className='bg-gradient-to-b from-black via-black to-gray-800 text-white w-full md:h-screen'>
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                        Portfolio
+                    </p>
+                    <p className="py-6">Check out some of my work right here</p>
                 </div>
 
-                <div  className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-                    {portfolio.map(({id, src}) => (
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+                    {portfolio.map(({ id, src }) => (
                         <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                             <img src={src} alt='' className='rounded-md duration-200 hover:scale-105' />
 
@@ -44,10 +45,7 @@ function Portfolio() {
                         </div>
                     ))}
                 </div>
-
-              
             </div>
-
         </div>
     );
 }
