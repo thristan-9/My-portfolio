1422a412ae57dcb57ab5dd5844289d52a1648dcd
thristan gabriel pereira deretti <thristanxyz@gmail.com>
2023-03-01 22:31:58 -0300

    save

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import whatsImg from '../assets/whatsImg.png';
 
 function Contact() {
     return (
-        <div name='contact' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
+        <div name='contact' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white'>
 
             <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
                 <div className='p-8'>
